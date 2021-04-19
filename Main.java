2700cb9c2b6289219e69d8404d6cdd962967bcdd
seulkioh91 <78770899+seulkioh91@users.noreply.github.com>
2021-04-19 17:15:33 +0900

    210419class

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        int finger = 10;
+        System.out.printf("Finger=[%5d]\n",finger);
+        
+        System.out.printf("Finger=[%-5d]\n",finger);
+        
+        System.out.printf("Finger=[%05d]\n",finger);
+    }
+}
